b65b7279ae52ae13b6771a8d3b1f73cebe8eaf72
Jay Prakash Mahto <jvjplus@gmail.com>
2020-10-17 09:42:36 +0530

    changed http to https

--- advanced/app.js
+++ advanced/app.js
@@ -9,10 +9,20 @@ GAME RULES:
 
 */
 
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/
+
 var score =[];
 var currentPlayer,currentScore;
 var isPlaying = true;
 var winningScore = 10;
+var firstRun=true;
 
 function init() {
     isPlaying = true;
@@ -132,9 +142,21 @@ function manageEvents() {
     newGameDOM.addEventListener("click", newGame);
 }
 
+function askForWinningScore(){
+    if(firstRun){
+        winningScore=prompt('Enter Winning Score');
+        // invalid input
+        if(!winningScore)
+            winningScore=20;
+        firstRun=false;
+    }
+}
+
 function main() {
     init();
+    setTimeout(askForWinningScore,100);
     manageEvents();
 }
+
 
 main();
